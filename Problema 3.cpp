0efaa5abfa909ee7fc154bfcefd6e64c24b4039d
Alex Ghinea <alex.ghinea@equilobe.com>
2017-10-27 23:44:36 +0300

    Update Problema 3.cpp

--- Problema 3.cpp
+++ Problema 3.cpp
@@ -29,17 +29,27 @@ public:
 std::vector<Activitate> activitati;
 std::vector<Activitate> activitatiCuProfitMaxim;
 
-void citesteActivitati(int nrActivitati)
+int citesteActivitati(int nrActivitati)
 {
+	int timpMaxim = 0;
 	for (int i = 0; i < nrActivitati; i++) {
 		int profit;
 		int timpLimita;
 
-		cout << "Profit: ";		cin >> profit;
-		cout << "Timp limita: "; cin >> timpLimita;
+		cout << "Profit: ";		
+		cin >> profit;
+
+		cout << "Timp limita: "; 
+		cin >> timpLimita;
+
+		if (timpLimita > timpMaxim)
+			timpMaxim = timpLimita;
+
 		Activitate activitateNoua(profit, timpLimita);
 		activitati.push_back(activitateNoua);
 	}
+
+	return timpMaxim;
 }
 
 bool cmd(const Activitate act2, const Activitate act1)
@@ -54,29 +64,47 @@ bool cmd(const Activitate act2, const Activitate act1)
 	int p2 = b.GetProfit();
 
 	if (t1 != t2)
-		return t1 > t2;
+		return t1 < t2;
 	return p1 < p2;
 }
 
 void afisareActivitati(std::vector<Activitate> activitati)
 {
+	cout << "\n";
 	for (int i = 0; i < activitati.size(); i++) {
 		cout << "Profit: " << activitati[i].GetProfit() << endl;
 		cout << "Timp Limita: " << activitati[i].GetTimpLimita() << endl;
 	}
 }
 
+void construiesteProfitMaxim(std::vector<Activitate> activitati)
+{
+	int timpActual = activitati[0].GetTimpLimita();
+	while (timpActual > 0)
+	{
+		activitatiCuProfitMaxim.push_back(activitati[0]);
+		activitati.erase(activitati.begin());
+		timpActual--;
+	}
+}
+
 int main()
 {
 	int nrActivitati;
+	int timpMaxim;
 	cout << "Nr Activitati: ";
 	cin >> nrActivitati;
 
-	citesteActivitati(nrActivitati);
+	timpMaxim = citesteActivitati(nrActivitati);
 
 	sort(activitati.begin(), activitati.end(), cmd);
 
 	afisareActivitati(activitati);
+
+	construiesteProfitMaxim(activitati);
+
+	afisareActivitati(activitatiCuProfitMaxim);
+
 	system("pause");
 	return 0;
 }
