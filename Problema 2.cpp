7f85a68a73794782566ce1e09547cf6b341f712c
Alex Ghinea <alex.ghinea@equilobe.com>
2017-10-29 00:58:11 +0300

    Update Problema 2.cpp

--- Problema 2.cpp
+++ Problema 2.cpp
@@ -1,5 +1,6 @@
 #include<iostream>
 #include<vector>
+#include<algorithm>
 
 using namespace std;
 
@@ -41,6 +42,33 @@ void citireTexte(int nrTexte)
 	}
 }
 
+void afisareTexte()
+{
+	cout << endl;
+	for (int i = 0; i < texte.size(); i++)
+	{
+		cout << "Lungime: " << texte[i].getLungime() << endl;
+		cout << "Frecventa: " << texte[i].getFrecventa() << endl;
+		cout << endl;
+	}
+}
+
+bool cmd(const Text t1, const Text t2)
+{
+	Text a(t1);
+	Text b(t2);
+
+	int l1 = a.getLungime();
+	int l2 = b.getLungime();
+
+	int f1 = a.getFrecventa();
+	int f2 = b.getFrecventa();
+
+	if (l1 != l2)
+		return l1 < l2;
+	return f1 < f2;
+}
+
 int main()
 {
 	int nrTexte;
@@ -48,6 +76,13 @@ int main()
 	cout << "Nr texte: ";
 	cin >> nrTexte;
 
+	citireTexte(nrTexte);
+
+	sort(texte.begin(), texte.end(), cmd);
+
+	afisareTexte();
+
+	system("pause");
 
 	return 0;
 }
